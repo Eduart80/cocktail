336415da8ecdcc686d573f5e32ef3db8b720af57
edd <zeedymarina@gmail.com>
2021-08-20 12:23:29 -0400

    4

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import CocktailList from "../Components/CocktailList";
 import SearchForm from "../Components/SearchList";
 
+
 const Home = () => {
   return (
     <main>
